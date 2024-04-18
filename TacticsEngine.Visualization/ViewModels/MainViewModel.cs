7e5484fa76e3f0f9b788ae047ba999d2c8bd432a
Derek Gooding <djgooding@live.com>
2024-04-18 09:02:53 -0400

    Added basic Reflection
    It's a start. Needs filtering and more comforting UI elements.

--- TacticsEngine.Visualization/ViewModels/MainViewModel.cs
+++ TacticsEngine.Visualization/ViewModels/MainViewModel.cs
@@ -1,6 +1,68 @@
-﻿namespace TacticsEngine.Visualization.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+using CaptainCoder.TacticsEngine.Board;
+
+namespace TacticsEngine.Visualization.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public static string Greeting => "This is Captain Coder's TacticsEngine!";
+
+    private Type? _selectedType;
+    public Type? SelectedType
+    {
+        get => _selectedType;
+        set
+        {
+            SetProperty(ref _selectedType, value);
+            GetSelectedTypeDetails();
+        }
+    }
+
+    #region ObservableCollections
+    public ObservableCollection<Type> ReflectionTypes { get; set; } = [];
+    public ObservableCollection<MethodInfo> Methods { get; set; } = [];
+    public ObservableCollection<FieldInfo> Fields { get; set; } = [];
+    public ObservableCollection<MemberInfo> Members { get; set; } = [];
+    #endregion
+
+    public MainViewModel()
+    {
+        GetClasses();
+    }
+
+    private void GetClasses()
+    {
+        ReflectionTypes.Clear();
+        Assembly? assembly = Assembly.GetAssembly(typeof(Board));
+        if (assembly == null) return;
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            ReflectionTypes.Add(type);
+        }
+    }
+
+    private void GetSelectedTypeDetails()
+    {
+        Methods.Clear();
+        Fields.Clear();
+        Members.Clear();
+        if (SelectedType == null) return;
+
+        foreach (MethodInfo method in SelectedType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+        {
+            Methods.Add(method);
+        }
+        foreach (FieldInfo field in SelectedType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+        {
+            Fields.Add(field);
+        }
+        foreach (MemberInfo member in SelectedType.GetMembers(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+        {
+            Members.Add(member);
+        }
+    }
 }
